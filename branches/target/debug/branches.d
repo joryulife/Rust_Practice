216b44e3d844ecc800470c897fd886004cfb2d70
joryulife <joryulife@gmail.com>
2021-05-06 17:02:16 +0900

    if文テスト

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/Users/t170076/lab/Rust_Practice/branches/target/debug/branches: /Users/t170076/lab/Rust_Practice/branches/src/main.rs
---
+++ branches/target/debug/deps/branches.d
@@ -0,0 +1,5 @@
+/Users/t170076/lab/Rust_Practice/branches/target/debug/deps/branches: src/main.rs
+
+/Users/t170076/lab/Rust_Practice/branches/target/debug/deps/branches.d: src/main.rs
+
+src/main.rs:
